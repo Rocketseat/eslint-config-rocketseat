0737962cd5cbd4805e1433d904c94f27154bf8b4
Diego Fernandes <diego.schell.f@gmail.com>
2022-05-20 09:26:26 -0300

    fix: Reorder eslint config

--- react.js
+++ react.js
@@ -7,8 +7,8 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'standard',
     'plugin:prettier/recommended',
-    'standard'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -24,6 +24,14 @@ module.exports = {
     '@typescript-eslint'
   ],
   rules: {
+    'prettier/prettier': ["error", {
+      'printWidth': 80,
+      'tabWidth': 2,
+      'singleQuote': true,
+      'trailingComma': 'all',
+      'arrowParens': 'always',
+      'semi': false,
+    }],
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     'jsx-a11y/alt-text': [
