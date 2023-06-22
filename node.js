75130fb1d55a8603bf2da15861f515625e61cb03
Leonardo William Lopes <leonardolopessocial@gmail.com>
2023-06-22 15:05:40 -0300

    Update node.js

--- node.js
+++ node.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
+    jest: true,
   },
   extends: ['standard', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
